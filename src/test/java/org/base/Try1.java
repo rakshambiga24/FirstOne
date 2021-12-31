a592667c9863b745372fd2f729cbd41ceaafc531
rakshambiga24 <rakshambiga93@gmail.com>
2021-12-31 14:46:11 +0530

    B1 changed

--- src/test/java/org/base/Try1.java
+++ src/test/java/org/base/Try1.java
@@ -6,6 +6,6 @@ public static void main(String[] args) {
 	System.out.println("Test2");
 	System.out.println("Test3");
 	System.out.println("Test4");
-	System.out.println("Test6");
+	System.out.println("Test5");
 }
 }
